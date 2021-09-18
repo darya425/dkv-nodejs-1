af6b14ac23c1869c3e42346a025640668a971e23
darya425 <dasha425@mail.ru>
2021-09-18 09:44:30 +0200

    fixed index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const contacts = require("./contacts");
 
-// contacts.listContacts();
-// contacts.getContactById(9);
-// contacts.removeContact(2);
-
-// contacts.addContact("Anatoli", "asdf@mail,ru", "333-333-333");
 const { Command } = require("commander");
 const program = new Command();
 program
